b26c05b30b3c1e24986727758606c50f656bd1d5
Andrius Mitkus <andrius.mitkus@gmail.com>
2017-12-21 11:30:38 +0200

    Evaluate integer expressions, clean up a bit

--- src/main/scala/com/lightbend/experiments/Generator.scala
+++ src/main/scala/com/lightbend/experiments/Generator.scala
@@ -5,6 +5,17 @@ import scala.annotation.tailrec
 
 object Generator {
 
+  private def parseInt(str: String) : Option[Int] = {
+    try {
+      if (str.startsWith("0x"))
+        Some(Integer.parseInt(str.substring(2), 16))
+      else
+        Some(str.toInt)
+    } catch {
+      case _: NumberFormatException => None
+    }
+  }
+
   // Helper token predicates
   private def id(name: String)(t: Token) = {
     t != null && t.getType == Token.IDENTIFIER && t.getText == name
@@ -20,22 +31,29 @@ object Generator {
   private val isStruct = id("struct") _
   private val isOpenBrace = tok("{") _
   private val isCloseBrace = tok("}") _
+  private val isPlus = tok("+") _
+  private def isNumber(t: Token) = t != null && t.getType == Token.NUMBER && parseInt(t.getText).isDefined
 
   // Higher level token concept with hierarchy and some semantics
-  sealed trait MetaToken
-  case class Empty() extends MetaToken
-  case class Single(t: Token) extends MetaToken
-  case class Enum(name: String, inside: MetaToken) extends MetaToken
-  case class Struct(name: String, inside: MetaToken) extends MetaToken
-  case class Scope(ts: Seq[MetaToken]) extends MetaToken
+  sealed trait Node
+  case class Empty() extends Node
+  case class Single(t: Token) extends Node
+  case class Number(n: Int) extends Node
+  case class Enum(name: String, inside: Node) extends Node
+  case class Struct(name: String, inside: Node) extends Node
+  case class Scope(ts: Seq[Node]) extends Node
 
-  def parseMetatokens(tokens: List[Token]) : List[MetaToken] = {
-    def parseScope(tokens: List[Token], acc: List[MetaToken]) : (Scope, List[Token]) = {
+  def parseNodes(tokens: List[Token]) : List[Node] = {
+
+    def parseScope(tokens: List[Token], acc: List[Node] = List.empty) : (Scope, List[Token]) = {
       tokens match {
         case a :: ts if isCloseBrace(a) => (Scope(acc.reverse), ts)
         case a :: ts if isOpenBrace(a) => {
-          val (scope, rest) = parseScope(ts, List.empty)
+          val (scope, rest) = parseScope(ts)
           parseScope(rest, scope :: acc)
+        }
+        case a :: ts if isNumber(a) => {
+          parseScope(ts, Number(parseInt(a.getText).get) :: acc)
         }
         case a :: ts => {
           parseScope(ts, Single(a) :: acc)
@@ -44,45 +62,67 @@ object Generator {
       }
     }
 
-    def itr(tokens: List[Token], acc: List[MetaToken]) : List[MetaToken] = {
+    def parse(tokens: List[Token], acc: List[Node] = List.empty) : List[Node] = {
       tokens match {
         case a :: b :: ts if isTypedef(a) && isEnum(b) => {
-          itr(ts, Enum("[unresolved]", Empty()) :: acc)
+          parse(ts, Enum("[unresolved]", Empty()) :: acc)
         }
         case a :: b :: ts if isTypedef(a) && isStruct(b) => {
-          itr(ts, Struct("[unresolved]", Empty()) :: acc)
+          parse(ts, Struct("[unresolved]", Empty()) :: acc)
         }
         case a :: b :: ts if isStruct(a) && isId(b) => {
-          itr(ts, Struct(b.getText, Empty()) :: acc)
+          parse(ts, Struct(b.getText, Empty()) :: acc)
         }
         case a :: ts if isOpenBrace(a) => {
-          val (scope, rest) = parseScope(ts, List.empty)
-          itr(rest, scope :: acc)
+          val (scope, rest) = parseScope(ts)
+          parse(rest, scope :: acc)
+        }
+        case a :: ts if isNumber(a) => {
+          parse(ts, Number(parseInt(a.getText).get) :: acc)
         }
         case a :: ts => {
-          itr(ts, Single(a) :: acc)
+          parse(ts, Single(a) :: acc)
         }
         case _ => acc
       }
     }
 
-    def resolve(metatokens: List[MetaToken]) : List[MetaToken] = {
-      metatokens match {
+    def resolve(nodes: List[Node], acc: List[Node] = List.empty) : List[Node] = {
+      nodes match {
         case Enum(_, _) :: Scope(inside) :: Single(name) :: ts => {
-          Enum(name.getText, Scope(inside)) :: resolve(ts)
+          resolve(ts, Enum(name.getText, Scope(inside)) :: acc)
         }
         case Struct(structname, _) :: Scope(inside) :: Single(name) :: ts if structname == "[unresolved]" => {
-          Struct(name.getText, Scope(inside)) :: resolve(ts)
+          resolve(ts, Struct(name.getText, Scope(inside)) :: acc)
         }
         case Struct(name, _) :: Scope(inside) :: ts => {
-          Struct(name, Scope(inside)) :: resolve(ts)
+          resolve(ts, Struct(name, Scope(inside)) :: acc)
         }
-        case t :: ts => t :: resolve(ts)
-        case _ => List.empty
+        case t :: ts => resolve(ts, t :: acc)
+        case _ => acc
       }
     }
 
-    resolve(itr(tokens, List.empty).reverse).reverse
+    def computeSums(nodes: List[Node], acc: List[Node] = List.empty) : List[Node] = {
+      nodes match {
+        case Number(a) :: Single(op) :: Number(b) :: ts if isPlus(op) => {
+          computeSums(ts, Number(a + b) :: acc)
+        }
+        case Scope(inside) :: ts => {
+          computeSums(ts, Scope(computeSums(inside.toList).reverse) :: acc)
+        }
+        case a :: ts => {
+          computeSums(ts, a :: acc)
+        }
+        case _ => acc
+      }
+    }
+
+    resolve(
+      computeSums(
+        parse(tokens).reverse
+      ).reverse
+    ).reverse
   }
 
   def generate(src : String) : Unit = {
@@ -91,17 +131,17 @@ object Generator {
     preprocessor.addWarning(Warning.IMPORT)
     preprocessor.addMacro("CURL_ISOCPP")
 
-    def toVec(f: Unit => Token): Vector[Token] = {
+    def toVec(f: Unit => Token): List[Token] = {
       @tailrec
-      def seq(acc : Vector[Token]) : Vector[Token] = {
+      def seq(acc : List[Token]) : List[Token] = {
         val tok = f()
         if (tok == null || tok.getType == Token.EOF)
           acc
         else
-          seq(acc :+ tok)
+          seq(tok :: acc)
       }
 
-      seq(Vector.empty)
+      seq(List.empty).reverse
     }
 
     preprocessor.addInput(new StringLexerSource(src, true))
@@ -110,7 +150,7 @@ object Generator {
       tp != Token.WHITESPACE && tp != Token.NL && tp != Token.CCOMMENT && tp != Token.CPPCOMMENT
     })
 
-    val enums = parseMetatokens(tokens.toList).filter( t => t.isInstanceOf[Enum] )
+    val enums = parseNodes(tokens).filter(t => t.isInstanceOf[Enum] )
 
     enums.foreach({
       case Enum(name, inside) => {
@@ -118,11 +158,11 @@ object Generator {
         inside.asInstanceOf[Scope].ts.foreach({
           case Single(t) if t.getText == "," => print("\n")
           case Single(t) => print(t.getText + " ")
+          case Number(n) => print("Int("+ n.toString + ") ")
         })
         println("\n}")
       }
       case _ =>
     })
-
   }
 }
